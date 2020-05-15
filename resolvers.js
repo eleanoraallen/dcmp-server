b98c13aee5fdae5931b1d81f9ac37d2816b54656
NA <nathanmichaelallen@gmail.com>
2020-05-15 11:02:09 -0400

    DOYOUBELIEVEINMIRACLESYESSSSSS

--- resolvers.js
+++ resolvers.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const db = mongoose.connection;
 const Map = require('./mongo-schema/map');
 const Point = require('./mongo-schema/point');
-const url = require('./authorization');
 
 // bind a function to perform when the database has been opened
 db.once('open', function () {
