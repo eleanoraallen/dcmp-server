d78f86ae8a46161cd1062f646d0a5f0932c51797
NA <nathanmichaelallen@gmail.com>
2020-05-15 01:08:42 -0400

    not gr8

--- authorization.js
+++ authorization.js
@@ -1,4 +1,4 @@
-// Add MongoDB URL here:
-const url = 'mongodb+srv://dcmp:eCzQkCUZPooOGmCA@dcmp-cluster-ynz04.mongodb.net/test?retryWrites=true&w=majority';
+// // Add MongoDB URL here:
+// const url = 'mongodb+srv://dcmp:eCzQkCUZPooOGmCA@dcmp-cluster-ynz04.mongodb.net/test?retryWrites=true&w=majority';
 
-module.exports = url;
+// module.exports = url;
