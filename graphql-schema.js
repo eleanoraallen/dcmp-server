dcf7d9ad201eddc3e871dae430d42b98a7408602
NA <nathanmichaelallen@gmail.com>
2020-05-18 00:17:52 -0400

    Update graphql-schema.js

--- graphql-schema.js
+++ graphql-schema.js
@@ -1,103 +1,103 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-    #A map
+    """ A Map """
     type Map {
-        #the ID of this map
+        """ The ID of this map """
         id: ID!
-        #The date on which this map was created
+        """ The date on which this map was created """
         createdAt: String!
-        #The name of this map
+        """ The name of this map """
         mapName: String
-        #The description for this map
+        """ The description for this map """
         description: String
-        #The creator of this map
+        """ The creator of this map """
         creatorName: String
     }
 
-    #Filter which can be applyed to mapList
+    """ Filter which can be applied to mapList """
     input MapListQuery {
-        #returns all maps with this id
+        """ Selects all maps with this ID """
         id: ID
-        #returns all maps with a given map name
+        """ Selects all maps with this mapName """
         mapName: String
-        #Returns all maps with a given creator name
+        """ Selects all maps with this creatorName """
         creatorName: String
-        #Returns all maps created after the given date
+        """ Selects all maps created after this date """
         startDate: String
-        #Returns all maps created before the given date
+        """ Selects all maps created before this date """
         endDate: String
-        #search operation to preform w/ fields (default AND)
+        """ MongoDB search operation to preform w/ the given fields (default AND) """
         operation: Operation
-        # set true to retrieve a psuedo-random selection of map (note: overides all other params except size, may return duplicate maps)
+        """ When true, returns a psuedo-random selection of maps (note: overides all other params except size, may return duplicate maps)"""
         random: Boolean
     }
 
-     #A point on a map
+     """ A point on a map """
      type Point {
-        #the ID of this point
+        """ The ID of this point """
         id: ID!
-        #The ID of the map with which the point is associated
+        """ The ID of the map with which this point is associated """
         mapId: ID!
-        #The name of this point
+        """ The name of this point """
         name: String!
-        #the coordinates of the point
+        """ The coordinates of the point """
         coordinates: Coordinates!
-        #The description for this point
+        """ The description of this point """
         description: String
-        #The category of this point
+        """ The category of this point. One of ART, MONUMENT, PUBLICSPACE, RESIDENCE, SCHOOL, BUISNESS, WORKPLACE, OTHER """
         category: String
-        #string to describe categories filled in as other
+        """ String used to describe a points category if it is OTHER """
         otherText: String
-        #The creator of this point
+        """ The creator of this point """
         creatorName: String
     }
 
-    #Filter which can be applyed pointList
+    """ Filter which can be applied to pointList """
     input PointListQuery {
-        # the ID of a point
+        """ Selects the point with this ID """
         id: ID
-        #The ID of the point's map
+        """ Selects all points with this mapID """
         mapId: ID
-        # coordinates of the point
+        """ Selects all maps with the given coordinants (inputed as an array of ints of length 2 in the form [x-coordinate, y-coordinate]) """
         coordinates: [Int!]
-        # set to retrieve all points within this distance of a given set of cordinates exclusive
+        """ Selects all points within this number of pixels from a given set of coordinates """
         within: Int
-        #name of the creator of the map
+        """ Selects all points with the given creatorName """
         creatorName: String
-        #the category of the point
+        """ Selects all points with the given category """
         category: String
-        #search operation to preform w/ fields (default AND)
+        """ MongoDB search operation to preform w/ the given fields (default AND) """
         operation: Operation
-        # set true to retrieve a psuedo-random selection of points (note: overides all other params except size, may return duplicate points)
+        """ When true, returns a psuedo-random selection of maps (note: overides all other params except size, may return duplicate maps)"""
         random: Boolean
     }
 
-    # stores the coordinates of a point
+    """ The coordinates of a point on the map """
     type Coordinates {
-        # the x coordinate of the point
+        """ The x coordinate of the point """
         x: Int!
-        # the y coordinate of the point
+        """ The y coordinate of the point """
         y: Int!
     }
 
-    # Input for adding a Point
+    """ Input for adding a point """
     input PointAdd {
-        #the name of the point
+        """ The name of this point """
         name: String!
-        #the coordinates of the point
+        """ The coordinates of this point (inputed as an array of ints of length 2 in the form [x-coordinate, y-coordinate]) """
         coordinates: [Int!]!
-        #The description for this point
+        """ The description of this point """
         description: String
-        #The category of this point
+        """ The category of this point. One of ART, MONUMENT, PUBLICSPACE, RESIDENCE, SCHOOL, BUISNESS, WORKPLACE, OTHER """
         category: String
-        #string to describe categories filled in as other
+        """ String used to describe a points category if it is OTHER """
         otherText: String
-        #The creator of this point
+        """ The creator of this point """
         creatorName: String
     }
 
-    # Search operations
+    """ MongoDB search operation """
     enum Operation {
         AND
         OR
@@ -105,71 +105,71 @@ const typeDefs = gql`
     }
 
     type Query {
-        #Given a map's id, returns that map
+        """ Given a maps ID, returns that map """
         map(id: ID!): Map
 
-        #Get the list maps of a given size starting at a given page conforming to a given filter
+        """ Get a list of maps of a given size, starting on a given page, conforming to a given order """
         mapList(
-            #query object for more specific searches
+            """ Query object for more specific searches """
             query: MapListQuery
-            #The number of maps to return (default 10)
+            """ The number of maps to return (default 10) """
             size: Int
-            #The index of the page to return (default 0)
+            """ The index of the page to return (default 0) """
             page: Int
         ): [Map]
 
-        #Given a points's id, returns that point
+        """ Given a point's ID, returns that point """
         point(id: ID!): Point
 
-        #Get the list points of a given size starting at a given page conforming to a given filter
+        """ Gets a list of points of a given size starting at a given page conforming to a given filter """
         pointList(
-            #query object for more specific searches
+            """ Query object for more specific searches """
             query: PointListQuery
-            #The number of points to return (default 10)
-            size: Int
-            #The index of the page to return (default 0)
+             """ The number of maps to return (default 10) """
+             size: Int
+            """ The index of the page to return (default 0) """
             page: Int
         ): [Point]
     }
 
     type Mutation {
-        #Used to add a map. Returns the ID of the added map
+        """ Used to add a map. Returns the ID of the added map """
         addMap(
-            #The name of this map
+            """ The name of this map """
             mapName: String
-            #The description for this map
-            description: String           
-            #The creator of this map
+            """ The description of this map """
+            description: String      
+            """ The creator of this map """
             creatorName: String
         ): ID
 
-        # used to add a point. Returns the ID of the added point
+        """ Used to add a point. Returns the ID of the added point """
         addPoint(
-            #The ID of the point's map
+            """ The ID of the point's map """
             mapId: ID!
-            #the name of this point
+            """ The name of this point """
             name: String!
-            #An array of ints of length 2 giving the [x, y] coordinates of the point
+            """ An array of ints of length 2 giving the [x, y] coordinates of the point """
             coordinates: [Int!]!
-            #The description for this point
+            """ The description of this point """
             description: String
-            #The category of this point 
+            """ The category of this point """ 
             category: String
-            # text to describe the category if it isn't one of the enumerated categories
+            """ text to describe the category if it isn't one of the enumerated categories """
             otherText: String
-            #The creator of this point
+            """ The creator of this point """ 
             creatorName: String
         ): ID
 
-        #(for use by the frontend) saves a map and associated points and returns maps ID 
+        """ (for use by the frontend) saves a map and associated points and returns maps ID  """ 
         saveMap(
-            #The name of this map
+            """ The name of this map  """
             mapName: String
-            #The description for this map
-            description: String           
-            #The creator of this map
+            """ The description for this map  """
+            description: String
+            """ The creator of this map  """
             creatorName: String
-            #points on this map
+            """ A list of the points to add  """
             points: [PointAdd]
         ): ID
     }
